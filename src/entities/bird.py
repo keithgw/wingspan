1c71095cc1790b7545a195327c5e2ca694f00d4d
Keith Williams <keith.gordon.williams@gmail.com>
2024-01-05 09:02:59 -0500

    include common name in bird class

--- src/entities/bird.py
+++ src/entities/bird.py
@@ -1,5 +1,6 @@
 class Bird:
-    def __init__(self, points, food_cost):
+    def __init__(self, common_name, points, food_cost):
+        self.common_name = common_name
         self.points = points
         self.food_cost = food_cost
 
--- tests/entities/test_bird.py
+++ tests/entities/test_bird.py
@@ -3,6 +3,7 @@ from src.entities.bird import Bird
 
 class TestBird(unittest.TestCase):
     def test_init(self):
-        bird = Bird(5, 2)
+        bird = Bird('Osprey', 5, 2)
+        self.assertEqual(bird.common_name, 'Osprey')
         self.assertEqual(bird.points, 5)
         self.assertEqual(bird.food_cost, 2)
