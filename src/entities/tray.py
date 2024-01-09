e4d61f97979340b47efa3d125b8f6e3241aecf38
Keith Williams <keith.gordon.williams@gmail.com>
2024-01-09 17:31:39 -0500

    implement bird tray that holds 3 birds and can flush.

---
+++ src/entities/tray.py
@@ -0,0 +1,48 @@
+from src.entities.bird import Bird
+
+class Tray:
+    def __init__(self):
+        self.birds = {}
+
+    def see_birds_in_tray(self):
+        '''Public method that returns a list of birds in the tray.'''
+        return [bird for bird in self.birds.keys()]
+
+    def draw_bird(self, common_name):
+        '''
+        Draw a bird from the tray.
+        
+        :param common_name: The common name of the bird to draw.
+        :type common_name: str
+        '''
+
+        if common_name not in self.birds:
+            raise ValueError(f"{common_name} does not exist in the tray.")
+        return self.birds.pop(common_name)
+    
+    def flush(self, discard_pile, bird_deck):
+        '''
+        Flush the tray and add all birds to the discard pile.
+        Refill the tray with birds from the deck.
+        
+        :param dicsard_pile: The discard pile.
+        :type deck: Deck
+        :param bird_deck: The bird deck
+        :type deck: Deck
+        '''
+
+        while len(self.birds) > 0:
+            discard_pile.add_card(self.birds.popitem()[1])
+        
+        while len(self.birds) < 3 and bird_deck.get_count() > 0:
+            bird = bird_deck.draw_card()
+            self.add_bird(bird)
+
+    def add_bird(self, bird):
+        '''
+        Add a bird to the tray.
+        
+        :param bird: The bird to add.
+        :type bird: Bird
+        '''
+        self.birds[bird.get_name()] = bird
---
+++ tests/entities/test_tray.py
@@ -0,0 +1,54 @@
+import unittest
+from src.entities.tray import Tray
+from src.entities.bird import Bird
+from src.entities.deck import Deck
+
+class TestTray(unittest.TestCase):
+    def setUp(self):
+        self.tray = Tray()
+        self.birds =  birds = [
+            Bird("Blue Jay", 5, 2),
+            Bird("Cardinal", 3, 1),
+            Bird("Barn Swallow", 3, 1)
+        ]
+
+    def test_see_birds_in_tray(self):
+        # Test when the tray is empty
+        self.assertEqual(self.tray.see_birds_in_tray(), [])
+
+        # Test when the tray has birds
+        for bird in self.birds:
+            self.tray.add_bird(bird)
+        self.assertEqual(self.tray.see_birds_in_tray(), [bird.get_name() for bird in self.birds]) # keys are returned in the order they were inserted
+
+    def test_draw_bird(self):
+        bird = self.birds[0]
+        self.tray.add_bird(bird)
+        drawn_bird = self.tray.draw_bird(bird.get_name())
+        self.assertEqual(drawn_bird, bird)
+        self.assertEqual(self.tray.see_birds_in_tray(), [])
+
+    def test_flush(self):
+        # Test when the deck is empty
+        discard_pile = Deck()
+        empty_deck = Deck()
+        self.tray.flush(discard_pile, empty_deck)
+        self.assertEqual(self.tray.see_birds_in_tray(), [])
+
+        # Test when the tray has birds
+        for bird in self.birds:
+            self.tray.add_bird(bird)
+        full_deck = Deck()
+        new_birds = [
+            Bird("Osprey", 5, 2),
+            Bird("Peregrine Falcon", 3, 1),
+            Bird("Anhinga", 4, 2)
+        ]
+        for bird in new_birds:
+            full_deck.add_card(bird)
+        self.tray.flush(discard_pile, full_deck)
+        self.assertEqual(self.tray.see_birds_in_tray(), [bird.get_name() for bird in new_birds[:3]])
+        self.assertEqual(discard_pile.get_count(), 3)
+
+if __name__ == '__main__':
+    unittest.main()
