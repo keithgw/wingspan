d8140fdc397de87ba4bd4f8f9e7922da6470e813
Keith Williams <keith.gordon.williams@gmail.com>
2024-01-13 18:33:54 -0500

    Remove magic from GameBoard, and make the number of slots available be a variable, 5 by default.

--- src/entities/gameboard.py
+++ src/entities/gameboard.py
@@ -1,9 +1,10 @@
 class GameBoard:
-    def __init__(self):
+    def __init__(self, limit=5):
+        self.limit = limit
         self.cards = []
 
     def check_if_full(self):
-        return len(self.cards) == 5
+        return len(self.cards) == self.limit
 
     def add_card(self, card):
         if self.check_if_full():
@@ -17,6 +18,6 @@ class GameBoard:
         print("{:<30s}{:<15s}{:<10s}".format("Bird Name", "Point Value", "Food Cost"))
         for bird in self.get_birds():
             print("{:<30s}{:<15d}{:<10d}".format(bird.get_name(), bird.get_points(), bird.get_food_cost()))
-        for _ in range(len(self.cards), 5):
+        for _ in range(len(self.cards), self.limit):
             print("{:<30s}{:<15s}{:<10s}".format("empty", "--", "--"))
         
