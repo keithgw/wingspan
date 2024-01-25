fa9bfadfe4fb29c7439bccf6a494a8d513a2775d
Keith Williams <keith.gordon.williams@gmail.com>
2024-01-25 10:17:31 -0500

    move phase constants to an external file that can be shared with reinforcement_learning.

--- src/entities/game_state.py
+++ src/entities/game_state.py
@@ -1,15 +1,13 @@
-from src.entities.deck import Deck
-from src.entities.tray import Tray
-from src.entities.birdfeeder import BirdFeeder
-from typing import List
-from src.entities.hand import BirdHand
-from typing import TYPE_CHECKING
+from src.constants import VALID_PHASES
+from typing import List, TYPE_CHECKING
 if TYPE_CHECKING:
     from src.entities.player import Player
+    from src.entities.deck import Deck
+    from src.entities.tray import Tray
+    from src.entities.birdfeeder import BirdFeeder
 
 class GameState:
-    VALID_PHASES = ['choose_action', 'choose_a_bird_to_play', 'choose_a_bird_to_draw']
-    def __init__(self, num_turns: int, bird_deck: Deck, discard_pile: Deck, tray: Tray, bird_feeder: BirdFeeder, players: List['Player']):
+    def __init__(self, num_turns: int, bird_deck: 'Deck', discard_pile: 'Deck', tray: 'Tray', bird_feeder: 'BirdFeeder', players: List['Player']):
         """
         Initialize a new game state with the minimum required elements to create a valid game state.
 
@@ -23,8 +21,8 @@ class GameState:
         """
         self.num_turns = num_turns
         self.num_players = len(players)
-        self.game_turn = 0 # increments by 1 each player turn, so will end at num_players * num_turns
-        self.phase = self.VALID_PHASES[0] # phases are intra-turn, so reset to 0 at the start of each player turn
+        self.game_turn = 0 # increments by 1 each player turn, so will end at num_players * num_turns #TODO: add to init
+        self.phase = self.VALID_PHASES[0] # phases are intra-turn, so reset to 0 at the start of each player turn #TODO: add to init and validate
         self.bird_deck = bird_deck
         self.discard_pile = discard_pile
         self.tray = tray
@@ -32,45 +30,58 @@ class GameState:
         self.players = players
 
     def get_num_turns(self) -> int:
-        '''Returns the total number of turns in the game.'''
+        """Returns the total number of turns in the game."""
         return self.num_turns
     
-    def get_bird_deck(self) -> Deck:
-        '''Returns the bird deck.'''
+    def get_game_turn(self) -> int:
+        """Returns the current game turn."""
+        return self.game_turn
+    
+    def get_phase(self) -> str:
+        """Returns the current phase."""
+        return self.phase
+    
+    def get_bird_deck(self) -> 'Deck':
+        """Returns the bird deck."""
         return self.bird_deck
     
-    def get_discard_pile(self) -> Deck:
-        '''Returns the discard pile.'''
+    def get_discard_pile(self) -> 'Deck':
+        """Returns the discard pile."""
         return self.discard_pile
     
     def get_tray(self) -> Tray:
-        '''Returns the bird tray.'''
+        """Returns the bird tray."""
         return self.tray
     
-    def get_bird_feeder(self) -> BirdFeeder:
-        '''Returns the bird feeder.'''
+    def get_bird_feeder(self) -> 'BirdFeeder':
+        """Returns the bird feeder."""
         return self.bird_feeder
     
     def get_players(self) -> List['Player']:        
-        '''Returns the list of players.'''
+        """Returns the list of players."""
         return self.players
     
     def get_player(self, idx: int) -> 'Player':
-        '''Returns the player at the given index. Players are indexed by initial turn order.'''
+        """
+        Returns the player at the given index. Players are indexed by initial turn order.
+        
+        Args:
+            idx (int): The index of the player to return.
+        """
         return self.players[idx]
 
     def get_current_player(self) -> 'Player':
-        '''Returns the current player'''
+        """Returns the current player"""
         player_idx = self.game_turn % self.num_players
         return self.players[player_idx]
 
     def end_player_turn(self, player: 'Player'):
-        '''
+        """
         Ends the given player's turn.
 
         Args:
             player (Player): The player whose turn is ending.
-        '''
+        """
         # check if the tray needs to be refilled
         if self.tray.is_not_full():
             self.tray.refill(self.bird_deck)
@@ -82,5 +93,5 @@ class GameState:
         self.phase = self.VALID_PHASES[0]
 
     def is_game_over(self) -> bool:
-        '''Returns True if the game is over, False otherwise.'''
+        """Returns True if the game is over, False otherwise."""
         return self.game_turn == self.num_turns * self.num_players
